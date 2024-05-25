224577786cf4339d1150e19f78e05129f9517517
Sarp Bozkurt <sarpbozk@gmail.com>
2024-05-25 20:50:08 +0300

    Initial Commit

---
+++ deeplinks demo/deeplinks_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  deeplinks_demoApp.swift
+//  deeplinks demo
+//
+//  Created by Sarp Bozkurt on 25.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct deeplinks_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
