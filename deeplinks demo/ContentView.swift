6f348233c51fdc40158ce9f7516e61763736aa4e
Sarp Bozkurt <sarpbozk@gmail.com>
2024-05-25 21:56:32 +0300

    initial commit

--- deeplinks demo/ContentView.swift
+++ deeplinks demo/ContentView.swift
@@ -7,18 +7,70 @@
 
 import SwiftUI
 
+
+enum NavigationRoute: Hashable {
+    case home
+    case detail
+}
+
 struct ContentView: View {
+    
+    @State private var navigationPath = NavigationPath()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack(path: $navigationPath) {
+            HomeView()
+                .navigationDestination(for: NavigationRoute.self) { route in
+                    switch route {
+                    case .home:
+                        HomeView()
+                    case .detail:
+                        DetailView()
+                    }
+                }
+                .onOpenURL { url in
+                    handleURL(url)
+                }
         }
-        .padding()
+    }
+    
+    private func handleURL(_ url: URL) {
+        if let host = url.host {
+            switch host {
+            case "home":
+                navigationPath.append(NavigationRoute.home)
+            case "detail":
+                navigationPath.append(NavigationRoute.detail)
+            default:
+                break
+            }
+        }
     }
 }
 
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("Home View")
+                .font(.largeTitle)
+                .padding()
+                .background(Color.red)
+                .foregroundColor(.white)
+        }
+    }
+}
+
+struct DetailView: View {
+    var body: some View {
+        VStack {
+            Text("Detail View")
+                .font(.largeTitle)
+                .padding()
+                .background(Color.green)
+                .foregroundColor(.white)
+        }
+    }
+}
 #Preview {
     ContentView()
 }
